2cdf341bed5e8958358a0f6d57d856c4cf708fff
sam-a-d <masamad.sust@gmail.com>
2024-12-29 18:13:28 +0100

    added all primary sensor simulators

--- SensorSimulation/simulator.py
+++ SensorSimulation/simulator.py
@@ -1,59 +1,84 @@
-import paho.mqtt.client as mqtt
+# A sensor simulator class
+
 import random
-import time
-import configparser
-import threading
 
+class Sensor:
+    '''
+        This module is responsible for producing the sensor data based on some specified criteria
+        Author's Github --> sam-a-d
+    '''
+    
+    def __init__(\
+                    self, electricity=(50,800),\
+                    water=(10,1000), natural_gas=(0, 1),\
+                    air_pollution=(10,500), crude_oil=(10,100),\
+                ):
+        
+        self.electricity = electricity
+        self.water = water
+        self.natural_gas = natural_gas
+        self.air_pollution = air_pollution
+        self.crude_oil = crude_oil
+    
+    # Resource utilization unit -------
+    # 1.
+    def getElectricity(self, *args):
 
-# Load MQTT configuration from file
-# config = configparser.ConfigParser()
-# config.read('config.ini')
+        self.electricity = args[0] if args else self.electricity
+        return round(random.uniform(*self.electricity), 2)
 
-# client_address = config['mqtt']['client_address']
-client_address = 'localhost'
-# port = int(config['mqtt']['port'])
-port = 1883
-areas = 1
-homes = 2
-sensors = ['electricity']
+    # 2. 
+    def getWater(self, *args):
 
-# forests = int(config['data_generation']['forests'])
-# areas = int(config['data_generation']['areas'])
-# time_sleep = int(config['data_generation']['time_sleep'])
-# sensors = config['data_generation']['sensors'].split('|')
+        self.water = args[0] if args else self.water
+        return round(random.uniform(*self.water), 2)
+    
+    # 3.
+    def getNaturalGas(self, *args):
 
-mqtt_client = mqtt.Client()
-mqtt_client.connect(client_address, port=port)
+        self.natural_gas = args[0] if args else self.natural_gas
+        return round(random.uniform(*self.natural_gas), 2)
 
-def publish_area_data(mqtt_client, sensors, area, home):
-    while True:
-        # Generate random sensor data
-        for sensor in sensors:
-            if sensor == 'electricity':
-                data = round(random.uniform(0, 40), 2)  # Simulating temperature between 0 to 40 degrees Celsius
-            # elif sensor == 'humidity':
-            #     data = round(random.uniform(30, 70), 2)  # Simulating humidity between 30% to 70%
-            # elif sensor == 'light_intensity':
-            #     data = random.randint(0, 1000)  # Simulating light intensity between 0 to 1000
-            # elif sensor == 'air_quality':
-            #     data = random.randint(0, 500)  # Simulating air quality index between 0 to 500
-            # -----> don't uncomment this -->  mqtt_client.publish(topic, payload=str(data))
-            topic = f"{sensor}/area_{area}/home_{home}"
-            mqtt_client.publish(topic, f'{{"{sensor}":{data}}}')
-            print(f"Published {topic}: {data}")
+    # 4.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    def getCrudeOil(self, *args):
 
-        time.sleep(1)  # Breaks between publishing data (defined in config)
-        # Uncomment the line below if you want to see the published data in the console
-        print(f"Published {sensor} data: {data}")
+        self.crude_oil = args[0] if args else self.crude_oil
+        return round(random.uniform(*self.crude_oil), 2)
+    
+    # 5.
+    def getAirPollution(self, *args):
 
-# Publish data for each sensor
-threads=[]
-for area in range(areas):
-    for home in range(homes):
-        thread = threading.Thread(target=publish_area_data, args=(mqtt_client, sensors, area, home))
-        threads.append(thread)
-        thread.start()
+        self.air_pollution = args[0] if args else self.air_pollution
+        return round(random.uniform(*self.air_pollution), 2)
+    
+    # the resouce production unit -------
 
-# Wait for all threads to complete (which they won't, as they're infinite loops)
-for thread in threads:
-    thread.join()
+    def __biased_random(self):
+        if random.random() < 0.8:  # 80% chance
+            return random.uniform(0, 0.5)  # Generate a number between 0-0.5
+        else:
+            return random.uniform(0.5, 1)  # Generate a number between 0.5-1
+
+    # 1. Solar energy production --> Linked with electricity consumption 
+    def getSolarProductionPercentage(self, *args):
+        
+        return self.__biased_random()
+
+    # 2. Hydrological energy production  --> Linked with electricity consumption
+    def getHydrologicalProductionPercentage(self):
+
+        return self.__biased_random()
+
+    # 3. Wind energy production --> Linked with electricity consumption
+    def getWindProductionPercentage(self, *args):
+        
+        return self.__biased_random()
+
+    # 4. Biogas production --> Linked with natural gas consumption
+    def bioGasProductionPercentage(self, *args):
+        
+        return self.__biased_random()
+
+    # 1-4 RES ---> Renewable Energy Production
+    # def renewableEnergyProduction(self, *args):
+    #     return self.__biased_random()

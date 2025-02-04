abbb24723be20b9d74fa5678b0d00555940f3699
sam-a-d <masamad.sust@gmail.com>
2025-02-04 11:23:02 +0200

    publisher --> getting sensor range from config.csv file if declared otherwise using the default value

--- SensorSimulation/mqtt-publisher.py
+++ SensorSimulation/mqtt-publisher.py
@@ -6,6 +6,8 @@ import threading
 from simulator import Sensor
 
 import csv
+import ast
+
 csvFilepath="/data/setting/config.csv"
 homesRecord="/data/homes/homes.csv"
 
@@ -38,6 +40,23 @@ def create_client():
     return client
 
 
+# Initialize a resource production range dictionary that store range data 
+res_prod_rngs = {}
+
+def convert_range(value):
+    """Convert range string from '(min-max)' to a tuple (min, max)"""
+    value = value.strip("() ")  # Remove parentheses and extra spaces
+    min_val, max_val = map(float, value.split('-'))  # Convert to float
+    return (min_val, max_val)
+
+with open(csvFilepath, 'r') as confFile:
+    reader = csv.DictReader(confFile)
+    for row in reader:
+        if '_range' in row['property']:
+            res_prod_rngs[row['property']] = convert_range(row['value'])
+
+
+
 def publish_area_data(sensors, area, home):
 
     mqtt_client = create_client()  # Create a new client for each thread
@@ -46,15 +65,15 @@ def publish_area_data(sensors, area, home):
         # Generate random sensor data
         for sensor in sensors:
             if sensor == 'electricity':
-                data = dataSimulator.getElectricity()
+                data = dataSimulator.getElectricity(res_prod_rngs['electricity_range']) if (val := res_prod_rngs.get('electricity_range')) else dataSimulator.getElectricity()
             elif sensor == 'water':
-                data = dataSimulator.getWater()
+                data = dataSimulator.getWater(res_prod_rngs['water_range']) if (val := res_prod_rngs.get('water_range')) else dataSimulator.getWater()
             elif sensor == 'natural_gas':
-                data = dataSimulator.getNaturalGas()
+                data = dataSimulator.getNaturalGas(res_prod_rngs['natural_gas_range']) if (val := res_prod_rngs.get('natural_gas_range')) else dataSimulator.getNaturalGas()
             elif sensor == 'air_pollution':
-                data = dataSimulator.getAirPollution()
+                data = dataSimulator.getAirPollution(res_prod_rngs['air_pollution_range']) if (val := res_prod_rngs.get('air_pollution_range')) else dataSimulator.getAirPollution()
             elif sensor == 'crude_oil':
-                data = dataSimulator.getCrudeOil()
+                data = dataSimulator.getCrudeOil(res_prod_rngs['crude_oil_range']) if (val := res_prod_rngs.get('crude_oil_range')) else dataSimulator.getCrudeOil()
             elif sensor == 'solarProduction':
                 data = dataSimulator.getSolarProduction()
             elif sensor == 'hydrologicalProduction':
